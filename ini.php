7d42f19094a3964a64fa33bce854015ae50e2ca5
Charles Marshall <charles@oneblackbear.com>
2013-12-09 12:29:41 +0000

    trying to get this to show up and be registered

--- ini.php
+++ ini.php
@@ -1,10 +1,7 @@
 <?php
-AutoLoader::register_assets("stylesheets/wildfire",__DIR__."/resources/public/stylesheets/wildfire/", "/*.css");
-AutoLoader::register_assets("javascripts/wildfire",__DIR__."/resources/public/javascripts/wildfire/", "/*.js");
-CMSApplication::register_asset("wildfire", "js", "mobile");
-CMSApplication::register_asset("wildfire", "css", "mobile");
 
-AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_assets("stylesheets/wildfire.interface.mobile",__DIR__."/resources/public/stylesheets", "/*.css");
+AutoLoader::register_assets("javascripts/wildfire.interface.mobile",__DIR__."/resources/public/javascripts", "/*.js");
 
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
