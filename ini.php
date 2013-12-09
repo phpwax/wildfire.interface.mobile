ec90bb6c04b386ee3fb1fb8f7a5813c4390f9a46
Charles Marshall <charles@oneblackbear.com>
2013-12-09 12:38:10 +0000

    changing path, trying to get the path to asset files correct

--- ini.php
+++ ini.php
@@ -1,7 +1,8 @@
 <?php
 
-AutoLoader::register_assets("stylesheets/wildfire.interface.mobile",__DIR__."/resources/public/stylesheets", "/*.css");
-AutoLoader::register_assets("javascripts/wildfire.interface.mobile",__DIR__."/resources/public/javascripts", "/*.js");
+AutoLoader::register_assets("javascripts/wildfire.interface.mobile",__DIR__."/assets/javascripts/wildfire.interface.mobile", "/*.js");
+AutoLoader::register_assets("stylesheets/wildfire.interface.mobile",__DIR__."/assets/stylesheets/wildfire.interface.mobile", "/*.css");
+AutoLoader::register_assets("images/wildfire.interface.mobile",__DIR__."/assets/images/wildfire.interface.mobile", "/*.png");
 
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
