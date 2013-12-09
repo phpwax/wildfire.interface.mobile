c0ea58bd211dd896facb7269c2500598af04cc5b
Charles Marshall <charles@oneblackbear.com>
2013-12-09 16:38:58 +0000

    add a global partial to turn off tinymce on mobiles / tablets

--- setup.php
+++ setup.php
@@ -1,8 +1,9 @@
 <?php
-#AutoLoader::register_view_path("plugin", __DIR__."/view/");
+
 CMSApplication::register_asset("wildfire", "js", "wildfire.interface.mobile");
 CMSApplication::register_asset("wildfire", "css", "wildfire.interface.mobile");
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.interface.mobile","dir"=>__DIR__);
 
-
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+CMSApplication::$global_partials[] = "_disable_tinymce";
 ?>
