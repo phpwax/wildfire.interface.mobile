c4b80518e6eff96b85d84e56e26b3cf76750517e
Charles Marshall <charles@oneblackbear.com>
2013-12-11 11:10:21 +0000

    overwrite to the partial view stack to force the cms to use this plugins version of _cms_meta as the stack order might not always be correct

--- setup.php
+++ setup.php
@@ -6,4 +6,13 @@ AutoLoader::$plugin_array[] = array("name"=>"wildfire.interface.mobile","dir"=>_
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 CMSApplication::$global_partials[] = "_disable_tinymce";
+
+WaxEvent::add("wax.after_plugin_view_paths", function(){
+  $template = WaxEvent::data();
+  //if this is cms_meta partial, need to move plugin version higher in the template stack
+  if($template->use_view == "_cms_meta"){
+    //we do this by basically removing the default interface one with this one
+    foreach($template->template_paths as $i=>$path) $template->template_paths[$i] = str_replace("/wildfire.interface/", "/wildfire.interface.mobile/", $path);
+  }
+});
 ?>
